d9f4fa62b8e57425ad711c6d0d75a4be6a281396
shigontsev <shigontsev1@mail.ru>
2023-03-12 12:28:30 +0400

    add validation, and fix calculate floating point number

--- CreditCalculator.PL.WebApp/ViewModels/LoanViewModel.cs
+++ CreditCalculator.PL.WebApp/ViewModels/LoanViewModel.cs
@@ -1,13 +1,32 @@
-﻿namespace CreditCalculator.PL.WebApp.ViewModels
+﻿using CreditCalculator.Entities;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Xml.Linq;
+
+namespace CreditCalculator.PL.WebApp.ViewModels
 {
-    public record class LoanViewModel
+    public record class LoanViewModel : IValidatableObject
     {
+        [Display(Name = "Сумма займа")]
+        [MinValue]
+        [Required(ErrorMessage = "Не указана Сумма займа")]
         public double Sum { get;  set; }
 
+        [Display(Name = "Срок займа")]
+        [MinValue]
+        [Required(ErrorMessage = "Не указан Срок займа")]
         public int Deadline { get; set; }
 
+        [Display(Name = "Ставка")]
+        [MinValue]
+        [Required(ErrorMessage = "Не указана Ставка")]
         public double Rate { get; set; }
 
+        [Display(Name = "Шаг платежа")]
+        [MinValue]
+        [Required(ErrorMessage = "Не указан Шаг платежа")]
         public int Step { get; set; } = 1;
 
         public LoanViewModel()
@@ -26,6 +45,60 @@
             Deadline = deadline;
             Rate = rate;
             Step = step;
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var errors = new List<ValidationResult>();
+
+            if (Sum <= 0) 
+            {
+                errors.Add(new ValidationResult(
+                    nameof(Sum) + " должна быть положительной > 0"));
+            }
+            if (Deadline <= 0)
+            {
+                errors.Add(new ValidationResult(
+                    nameof(Deadline) + " должен быть положительным > 0"));
+            }
+            if (Rate <= 0)
+            {
+                errors.Add(new ValidationResult(
+                    nameof(Rate) + " должна быть положительной > 0"));
+            }
+            if (Step <= 0)
+            {
+                errors.Add(new ValidationResult(
+                    nameof(Step) + " должен быть положительным > 0"));
+            }
+            if (Step > Deadline)
+            {
+                errors.Add(new ValidationResult(
+                    nameof(Step) + " должен быть  <= Срока займа"));
+            }
+
+            return errors;
+        }
+    }
+    public class MinValueAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value is int valueInt)
+            {
+                if (valueInt > 0)    // если имя не равно admin
+                    return true;
+                else
+                    ErrorMessage = "Значение  должно быть > 0";
+            }
+            if (value is double valueDouble)
+            {
+                if (valueDouble > 0)    // если имя не равно admin
+                    return true;
+                else
+                    ErrorMessage = "Значение должно быть > 0";
+            }
+            return false;
         }
     }
 }
